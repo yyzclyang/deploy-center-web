77a54a4d26a715db5b2527d3ca52728c0d15255e
yyzcl <yyzclyang@outlook.com>
2021-04-15 12:40:39 +0800

    feature(router): 创建 router

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,22 @@
 import { FC } from 'react';
-import style from './app.module.scss';
+import { Router, Route, Switch, Redirect } from 'react-router';
+import history from './utils/history';
+import Login from './pages/login';
+import Home from './pages/home';
+import './app.module.scss';
 
 const App: FC = () => (
-  <>
-    <h1 className={style.title}>welcome to deploy center</h1>
-  </>
+  <Router history={history}>
+    <Switch>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Redirect to="/home" />
+    </Switch>
+  </Router>
 );
 
 export default App;
